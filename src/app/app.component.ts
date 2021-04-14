0c4ba53babaf00c90cecd6a595181ee77876b722
Vivek Singh <viveksuarez7@gmail.com>
2021-04-14 14:26:25 +0530

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,48 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pw';
+  length=0;
+  includeNumbers = false;
+  includeLetters = false;
+  includeSymbols=false;
+  password='';
+  onChangeLength(value:string){
+    const parseVal = parseInt(value);
+    if(!isNaN(parseVal)){
+      this.length=parseVal;
+    }
+  }
+  onChangeUseLetters(){
+    this.includeLetters=!this.includeLetters;
+  }
+  onChangeUseNumbers(){
+    this.includeNumbers=!this.includeNumbers;
+  }
+  onChangeUseSymbols(){
+    this.includeSymbols=!this.includeSymbols;
+  }
+ onButtonClick(){
+   const letters='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+   const numbers='1234567890'
+   const symbols='!@#$%^&*()'
+
+   
+
+   let validChairs = '';
+   if(this.includeLetters){
+     validChairs += letters;
+   }
+   if(this.includeNumbers){
+     validChairs += numbers; 
+   }
+   if(this.includeSymbols){
+     validChairs += symbols;
+   }
+   let generatedPassword = '';
+   for(let i=0;i<this.length;i++){
+     const index = Math.floor(Math.random()*validChairs.length);
+     generatedPassword += validChairs[index];
+   }
+   this.password=generatedPassword;
+ }
 }
